17f1e683691997d92e1028f2f890526ba606dabc
Goran Mekić <meka@tilda.center>
2019-05-09 03:01:30 +0200

    Initial commit

---
+++ shell/provision.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+MY_PATH=`dirname $0`
+
+pkg install -y ccache xorg font-adobe-100dpi gmake git-lite
+cp ${MY_PATH}/.cshrc ~devel/.cshrc
+chown devel:devel ~devel/.cshrc
+
+if [ ! -d /usr/src/fisean ]; then
+  git clone https://github.com/fisean/fisean /usr/src/fisean
+  chown -R devel:devel /usr/src/fisean
+fi
+
+if [ ! -d /usr/src/gnui ]; then
+  git clone https://github.com/fisean/gnui /usr/src/gnui
+  chown -R devel:devel /usr/src/gnui
+fi
